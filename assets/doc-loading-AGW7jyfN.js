2eb2d2597ab744b36c5ce49a4087cd735ac14e37
Issac <Issac>
2024-01-16 19:47:13 +0800

    perf: caching doid resolving

---
+++ assets/doc-loading-AGW7jyfN.js
@@ -0,0 +1,19 @@
+const e=`import { customElement, ThemeElement, html } from '@riffian-web/ui/shared/theme-element'\r
+// Components\r
+import '@riffian-web/ui/loading/icon'\r
+import '@riffian-web/ui/loading/skeleton'\r
+\r
+@customElement('doc-loading')\r
+export class DocLoading extends ThemeElement('') {\r
+  override render() {\r
+    return html\`\r
+      <div class="flex gap-4">\r
+        <i>icon:</i>\r
+        <loading-icon></loading-icon>\r
+        <i>skeleton:</i>\r
+        <loading-skeleton num="3"></loading-skeleton>\r
+      </div>\r
+    \`\r
+  }\r
+}\r
+`;export{e as default};
